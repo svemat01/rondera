071cc5a2cb9df4ce29dc09cc2242d27d66a284d0
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-03-19 22:32:10 +0000

    Add BigInt.toJSON

--- src/hooks/server.ts
+++ src/hooks/server.ts
@@ -4,6 +4,8 @@ import { setupRedis } from '$lib/server/cache/redisCache.js';
 
 import { checkUser } from './users.server.js';
 
+BigInt.prototype.toJSON = function() { return this.toString() }
+
 if (!building) {
     await setupDB();
     await setupRedis();
