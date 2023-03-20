3e963c9450579e78bfb278076c7fc67f78ebe119
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-03-20 22:08:44 +0000

    Introduce basic design

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import icon from '$lib/assets/icon.svg';
+</script>
+
+<main>
+    <div class="branding">
+        <img src={icon} alt="Logo" />
+        <h1>RONDERA</h1>
+    </div>
+    <div class="separator" />
+    <slot />
+</main>
+
+<style lang="scss">
+    main {
+        display: grid;
+        grid-template-columns: 1fr 2px 1fr;
+        align-items: center;
+        justify-items: center;
+        gap: 1rem;
+
+        min-height: 100vh;
+        max-width: 1300px;
+
+        margin-inline: auto;
+    }
+
+    .separator {
+        width: 100%;
+        background-color: #ccc;
+        height: 60%;
+    }
+
+    .branding {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        h1 {
+            font-size: 5.5rem;
+            font-weight: 400;
+
+            font-family: 'Jura', sans-serif;
+        }
+
+        img {
+            width: 100px;
+        }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+    import '@fontsource/jura/400.css';
+    import '@fontsource/inter/variable.css';
     import '../app.scss';
 </script>
 
